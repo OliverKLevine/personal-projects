ea96ee05daf8c133da6d56b547a55819b5cfb99a
OliverKG <116015506+OliverKG@users.noreply.github.com>
2023-09-05 15:50:23 -0400

    edit

--- qualtrics/parseqsf.py
+++ qualtrics/parseqsf.py
@@ -2,22 +2,24 @@ import os
 import sys
 import json
 
-def main(qsf_json, out_qsf):
+def main(qsf_json, out_qsf, switch = False):
+    if switch:
+        qsf_json, out_qsf = out_qsf, qsf_json
     with open(qsf_json, "r") as input:
         qsf = json.load(input)
     
-    qsf["SurveyEntry"]["SurveyID"] = None
-    qsf["SurveyEntry"]["SurveyName"] = None
-    qsf["SurveyEntry"]["SurveyOwnerID"] = None
-    qsf["SurveyEntry"]["CreatorID"] = None
-    qsf["SurveyEntry"]["SurveyActiveResponseSet"] = None
-
-    for x in range(len(qsf["SurveyElements"])-1,0,-1):
-        if qsf["SurveyElements"][x]["Element"] == "NT":
-            qsf["SurveyElements"].pop(x)
+    if not switch:
+        qsf["SurveyEntry"]["SurveyID"] = "SV_cHXMOV5X1K2nWgm"
+        for x in range(len(qsf["SurveyElements"])-1,-1,-1):
+            qsf["SurveyElements"][x]["SurveyID"] = "SV_cHXMOV5X1K2nWgm"
     
-    with open(out_qsf,"w") as output:
-        json.dump(qsf, output)
+        with open(out_qsf,"w") as output:
+            json.dump(qsf, output)
+    else:
+        with open(out_qsf,"w") as output:
+            json.dump(qsf, output, indent=4)
+
+
 
 if __name__ == "__main__":
     main(*sys.argv[1:])
