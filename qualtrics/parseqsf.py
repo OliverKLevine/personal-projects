a9ea5c49ee660b3310bd1252b94a8040761aec8f
Oliver Levine <oliverklevine@uga.edu>
2023-11-07 01:07:50 -0500

    got it working kinda

--- qualtrics/parseqsf.py
+++ qualtrics/parseqsf.py
@@ -12,10 +12,12 @@ class Survey:
             self.__dict__[name] = self.qsf["SurveyEntry"][item]
         self.elements = getdict(self.qsf,["SurveyElements"])
         self.blocks = self.Blocks(self)
+        self.flow = self.Flow(self)
         self.count = self.Question_Count(self)
         if not keep_questions:
             self.blocks.empty()
             self.count.empty()
+            self.flow.empty()
             self.questions = []
         else: self.questions = [element for element in self.elements if element["Element"] == "SQ"]
         setdict(self.qsf,["SurveyElements"],[element for element in self.elements if element["Element"] != "SQ"])
@@ -30,11 +32,10 @@ class Survey:
 
         self.blocks.extract()
         self.count.extract()
+        self.flow.extract()
         for x in range(len(self.questions)):
             if isinstance(self.questions[x],self.Question):
                 self.questions[x] = self.questions[x].extract()
-
-        print(json.dumps(self.qsf, indent=4))
 
     class Blocks:
         def __init__(self, survey):
@@ -44,21 +45,25 @@ class Survey:
             self.__dict__.update(self.survey.elements[self.index])
         
         def empty(self):
-            self.Payload = [block for block in self.Payload if block["Type"] == "Trash"]
+            trash = [block for block in self.Payload if block["Type"] == "Trash"]
+            self.Payload = {}
+            if len(trash) > 0:
+                self.Payload["0"] = trash[0]
 
         def add_block(self, block_text):
             name = block_text.split("]]")[0].strip()
-            info = [block for block in self.Payload if block["Description"] == name]
+            info = [self.Payload[blindex] for blindex in self.Payload if self.Payload[blindex]["Description"] == name]
             if len(info) > 0: info = info[0]
             else: info = {
                 "Description":name,
-                "Type":"Default",
+                "Type":"Standard",
                 "BlockElements":[]
             }
             description = block_text.split("]]")[1].split("[[")[0].strip()
             if description: pass#add description later
+            strindex = str(len(self.Payload))
             if not "ID" in info:
-                info["ID"] = f'BL_{"".join(["0" for x in range(15-len(str(len(self.Payload))))])}{len(self.Payload)}'
+                info["ID"] = f'BL_{"".join(["0" for x in range(15-len(strindex))])}{len(self.Payload)}'
 
             try: 
                 block_text = block_text.split("[[Question:")[1:]
@@ -71,7 +76,32 @@ class Survey:
                 question = self.survey.Question(self.survey,info,question)
                 info["BlockElements"].append(question.block_info)
 
-            self.Payload.insert(-1,info)
+            self.Payload[strindex] = info
+            self.survey.flow.add_block(info)
+            
+
+        def extract(self):
+            index = self.__dict__.pop("index")
+            self.__dict__.pop("survey").elements[index] = self.__dict__
+
+    class Flow:
+        def __init__(self, survey):
+            self.survey = survey
+            abbrev = "FL"
+            self.index = self.survey.elements.index([element for element in self.survey.elements if element["Element"] == abbrev][0])
+            self.__dict__.update(self.survey.elements[self.index])
+        
+        def empty(self):
+            self.Payload["Flow"] = []
+            self.Payload["Properties"]["Count"] = 1
+
+        def add_block(self, block):
+            self.Payload["Flow"].append({
+                "ID": block["ID"],
+                "Type": "Block",
+                "FlowID": f"FL_{int(block['ID'].split('_')[-1])+1}"
+            })
+            self.Payload["Properties"]["Count"] += 1
             
 
         def extract(self):
@@ -101,7 +131,7 @@ class Survey:
         def __init__(self, survey,block_info,question_text):
             self.survey = survey
             question_types = question_text.split("]]")[0].split(":")
-            qtype = {"MC":"MC","TE":"TE","CS":"CS","ConstantSum":"CS","Slider":"Slider","Text":"DB"}[question_types[0]]
+            qtype = {"MC":"MC","TE":"TE","CS":"CS","ConstantSum":"CS","Slider":"Slider","Text":"DB","Matrix":"Matrix"}[question_types[0]]
 
             self.count = survey.count.plus()
             self.name = question_text.split("\n")[1].strip()
@@ -136,7 +166,8 @@ class Survey:
                     "NotApplicable": False,
                     "MobileFirst": True
                 },
-                "CS":{"QuestionDescriptionOption": "UseText"}
+                "CS":{"QuestionDescriptionOption": "UseText"},
+                "DB":{"QuestionDescriptionOption": "UseText"}
             }
 
             self.info = {
@@ -165,7 +196,7 @@ class Survey:
                 }
             }
             self.Payload = getdict(self.info,["Payload"])
-            if qtype == "CS": self.Payload["SubSelector"] = "TX"
+            if qtype == "CS" or qtype == "MC": self.Payload["SubSelector"] = "TX"
 
             if "[[Choices]]" in question_text:
                 choices = [choice.strip() for choice in question_text.split("[[Choices]]")[1].split("[[")[0].split("\n") if choice.strip()]
@@ -173,6 +204,8 @@ class Survey:
                 orders = []
                 for x in range(len(choices)):
                     choice_dict[str(x+1)] = {"Display":choices[x]}
+                    if choices[x].strip().lower() == "other (specify below)":
+                        choice_dict[str(x+1)].update({"TextEntry": "true","TextEntrySize": "Small"})
                     orders.append(str(x+1))
                 self.Payload["Choices"] = choice_dict
                 self.Payload["ChoiceOrder"] = orders
@@ -189,7 +222,7 @@ class Survey:
 
             self.block_info = {
                 "Type":"Question",
-                "QuestionID":self.PrimaryAttribute
+                "QuestionID":self.info["PrimaryAttribute"]
             }
             self.survey.elements.append(self.info)
 
@@ -202,13 +235,13 @@ class Survey:
 
 def main(blank_qsf, input_questions, out_qsf = False, keep_questions = False):
     keep_questions = relaxed_bool(keep_questions)
-    if not out_qsf: out_qsf = blank_qsf
+    if not out_qsf: out_qsf = input_questions.replace(".txt",".qsf")
     
     survey = Survey(blank_qsf, input_questions, keep_questions)
-        
-    quit()
+
+
     with open(out_qsf,"w") as output:
-        json.dump(survey.qsf, output)
+        json.dump(survey.qsf, output, indent=4)
     
 
     
